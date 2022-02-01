07159021c2f66a2f976437ebf3795a4fdc91b15d
Sebastian <sebastian.frey@outlook.com>
2022-02-01 09:13:51 +0000

    test. add more json schema tests

--- src/main/java/com/github/sebastianfrey/joa/schemas/type/PatternType.java
+++ src/main/java/com/github/sebastianfrey/joa/schemas/type/PatternType.java
@@ -3,6 +3,8 @@ package com.github.sebastianfrey.joa.schemas.type;
 public class PatternType {
   private String pattern;
 
+  public PatternType() {}
+
   public PatternType(String pattern) {
     this.pattern = pattern;
   }
